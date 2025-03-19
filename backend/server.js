9a649318bd4a4b87124b60ff6ef3337a7249b48c
Floyd Carlo <codeit69420@gmail.com>
2025-03-19 22:40:21 +0530

    done

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,7 @@ app.use(
     origin: ["http://localhost:5173", "http://localhost:5179"], // Your frontend URLs
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
   })
 );
 
